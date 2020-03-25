70f1d385de315cd7936001caea2a2e16ec22fa88
Carlos A Jaime <cajaime@miners.utep.edu>
2020-03-25 13:17:38 -0600

    Buttons work on expansion board

--- project/toy/switches.c
+++ project/toy/switches.c
@@ -2,24 +2,24 @@
 #include "switches.h"
 #include "led.h"
 
-char switch_state_down, switch_state_changed;
+char switch_state_down, switch_state_changed, switch1_down, switch2_down, switch3_down;
 
 static char
 switch_update_interrupt_sense()
 {
-  char p1val = P1IN;
-  P1IES |= (p1val & SWITCHES);
-  P1IES &= (p1val | ~SWITCHES);
-  return p1val;
+  char p2val = P2IN;
+  P2IES |= (p2val & SWITCHES);
+  P2IES &= (p2val | ~SWITCHES);
+  return p2val;
 }
 
 void
 switch_init()
 {
-  P1REN |= SWITCHES;
-  P1IE = SWITCHES;
-  P1OUT |= SWITCHES;
-  P1DIR &= ~SWITCHES;
+  P2REN |= SWITCHES;
+  P2IE = SWITCHES;
+  P2OUT |= SWITCHES;
+  P2DIR &= ~SWITCHES;
   switch_update_interrupt_sense();
   led_update();
 }
@@ -27,8 +27,12 @@ switch_init()
 void
 switch_interrupt_handler()
 {
-  char p1val = switch_update_interrupt_sense();
-  switch_state_down = (p1val & SW1) ? 0 : 1;
+  char p2val = switch_update_interrupt_sense();
+  switch_state_down = (p2val & SW0) ? 0 : 1;
+  switch1_down = (p2val & SW1) ? 0 : 1;
+  switch2_down = (p2val & SW2) ? 0 : 1;
+  switch3_down = (p2val & SW3) ? 0 : 1;
+  
   switch_state_changed = 1;
   led_update();
 }
--- project/toy/switches.h
+++ project/toy/switches.h
@@ -1,12 +1,15 @@
 #ifndef switches_included
 #define switches_included
 
-#define SW1 BIT3
-#define SWITCHES SW1
+#define SW0 BIT0
+#define SW1 BIT1
+#define SW2 BIT2
+#define SW3 BIT3
+#define SWITCHES (SW0 | SW1 | SW2 | SW3)
 
 void switch_init();
 void switch_interrupt_handler();
 
-extern char switch_state_down, switch_state_changed;
+extern char switch_state_down, switch_state_changed, switch1_down, switch2_down, switch3_down;
 
 #endif
